19734cebfc783890803412f5a6f7e8a1c0ebc438
amal <amal_266@icloud.com>
2024-06-30 14:07:53 +0300

    using grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,26 @@
     </title>
 </svelte:head>
 
-<div class=" bg-red-100 h-[23rem] w-[19rem] rounded-lg flex justify-center items-center ">
-    <div class=" bg-black h-[5rem] w-[5rem] rounded-full ">
-
-    </div>
+<div class=" bg-red-100 min-h-[23rem] w-[19rem] rounded-lg grid grid-cols-4 gap-1">
+  <button>%</button> 
+  <button>√</button> 
+  <button>CE</button>
+  <button>C</button>
+  <button>7</button>
+  <button>8</button>
+  <button>9</button>
+  <button>-</button>
+  <button>4</button>
+  <button>5</button>
+  <button>6</button>
+  <button>÷</button>
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button>×</button>
+  <button>.</button>
+  <button>0</button>
+  <button>=</button>
+  <button>+</button>
 </div>
 
